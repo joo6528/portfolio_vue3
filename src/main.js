fca0dbdd8380e43957811ddbd05ac632a57fe97d
Juhyeon <qlswngus11@naver.com>
2026-05-03 17:22:15 +0900

    no message

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia' 
 import '@/assets/css/base.css'
 import 'aos/dist/aos.css'
-// import AOS from 'aos'
+import AOS from 'aos'
 import App from '@/App.vue'
 import router from '@/router'
 
@@ -12,6 +12,6 @@ createApp(App)
   .mount('#app')
 
 // AOS 초기화
-// AOS.init({
-//   duration: 400, // 애니메이션 시간
-// })
+AOS.init({
+  duration: 400, // 애니메이션 시간
+})
